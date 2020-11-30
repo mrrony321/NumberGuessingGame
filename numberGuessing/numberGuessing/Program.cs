ce6920dc9dbba9c1e4e5d785dc556fe6c0711215
Mizanur Rahman Rony <rmizanurrahman76@gmail.com>
2020-11-30 11:38:33 +0600

    number guessing 1.0
    a console app using C# as a fun project inspired by traversy media

---
+++ numberGuessing/numberGuessing/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace numberGuessing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+
+            Console.WriteLine("what is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hi {0}, Welcome to the game...", name);
+            //Console.WriteLine("")
+            //declareing winnig variable;
+            int winningNumber = 4;
+
+            //taking inputs
+            Console.WriteLine("Please enter a number between 1-10");
+            Console.ForegroundColor = ConsoleColor.Green;
+            int inputNumber = Convert.ToInt32(Console.ReadLine());
+            //Console.ResetColor();
+
+            while(inputNumber != winningNumber)
+            {
+                Console.WriteLine("Please enter a number between 1-10");
+                Console.ForegroundColor = ConsoleColor.Green;
+                inputNumber = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Win!!!!Win!!!Win!!");
+            Console.ResetColor();
+            //if()
+
+
+
+        }
+    }
+}
